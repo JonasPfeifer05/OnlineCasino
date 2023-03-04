f8611e731f4816eb9332ae1f13400f416fc0a862
jonas <jonaspfeifer@drei.at>
2023-03-04 20:30:30 +0100

    basic login method to create access_token and refresh_token, to refresh the access_token and authorize a token. Access tokens lasts 10min and can be refreshed for 8h without logging in!

--- server/src/users/users.router.ts
+++ server/src/users/users.router.ts
@@ -1,6 +1,7 @@
 import {Router} from "express";
 import {UsersService} from "./users.service";
 import {User} from "./user";
+import {UserToken} from "./user-token";
 
 export class UsersRouter {
     router: Router
@@ -21,11 +22,30 @@ export class UsersRouter {
 
         this.router.post("/get", async (req, res) => {
             let error: Error|undefined;
-            let user: User = await this.service.getUser(req.body).catch(reason => error = reason);
 
+            let userData: UserToken = await this.service.authenticateJWT(req.headers.authorization, process.env.JWT_AUTH_TOKEN).catch(reason => error = reason);
             if (error) return res.status(400).json({message: error.message});
 
+            let user: User = await this.service.getUser(userData as User).catch(reason => error = reason);
             res.status(200).json(user);
+        })
+
+        this.router.post("/login", async (req, res) => {
+            let error: Error | undefined;
+
+            let [access_token, refresh_token] = await this.service.login(req.body).catch(reason => error = reason);
+            if (error) return res.status(400).json({message: error.message});
+
+            res.status(200).json({access_token, refresh_token});
+        })
+
+        this.router.post("/refresh", async (req, res) => {
+            let error: Error | unknown;
+
+            let token: string = await this.service.refreshToken(req.body.refresh_token).catch(reason => error = reason);
+            if (error) return res.status(400).json({message: (error as Error).message});
+
+            res.status(200).json({token: token});
         })
     }
 }
--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -1,5 +1,8 @@
 import {User} from "./user";
 import {UsersStorage} from "./users.storage";
+import {UserToken} from "./user-token";
+
+const jwt = require("jsonwebtoken")
 
 export class UsersService {
 
@@ -43,6 +46,61 @@ export class UsersService {
         if (!user) throw new Error("Failed to create User!");
 
         return user;
+    }
+
+    async generateJWT(data: UserToken, token: string|undefined, expire: string): Promise<string> {
+        if (!token) throw new Error("Internal Token error!");
+        if (!data.email || !data.password) throw new Error("Not enough data passed!");
+
+        let error: Error|undefined;
+        await this.getUser(data as User).catch(reason => error = reason);
+
+        if (error) throw error;
+
+        return jwt.sign(data, token, {expiresIn: expire});
+    }
+
+    async authenticateJWT(authentication_header: string|undefined, token: string|undefined): Promise<UserToken> {
+        if (!token) throw new Error("Internal Token error!");
+        if (!authentication_header) throw new Error("No Auth Header set!");
+
+        let error: Error|undefined;
+        let user: UserToken|undefined;
+        jwt.verify(authentication_header, token, (errorR: any, decoded: any) => {
+            error = errorR;
+            user = decoded;
+        })
+
+        if (error || !user) throw new Error("Token verification failed!");
+
+        return user;
+    }
+
+    async login(data: UserToken): Promise<[string, string]> {
+        let accessToken: string | undefined;
+        let refreshToken: string | undefined;
+
+        let error: Error|undefined;
+
+        accessToken = await this.generateJWT(data, process.env.JWT_AUTH_TOKEN, "10min").catch(reason => error = reason);
+        if (error || !accessToken) throw error;
+        refreshToken = await this.generateJWT(data, process.env.JWT_REFRESH_TOKEN, "8h").catch(reason => error = reason);
+        if (error || !refreshToken) throw error;
+
+        return [accessToken, refreshToken]
+    }
+
+    async refreshToken(refresh_token: string | undefined) {
+        let error: Error|undefined;
+
+        let refresh: UserToken = await this.authenticateJWT(refresh_token, process.env.JWT_REFRESH_TOKEN).catch(reason => error = reason);
+        if (error) throw error;
+
+        let userData: UserToken = {email: refresh.email, password: refresh.password}
+        let token = await this.generateJWT(userData, process.env.JWT_AUTH_TOKEN, "10min").catch(reason => error = reason);
+        if (error) throw error;
+
+        return token
     }
 }
 
