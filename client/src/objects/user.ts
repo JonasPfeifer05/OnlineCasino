52a6c00eb2e47990b98ca62262f5041f7631b3a7
jonas <jonaspfeifer@drei.at>
2023-03-05 19:06:03 +0100

    added methodes to validate and refresh the access token

--- client/src/objects/user.ts
+++ client/src/objects/user.ts
@@ -6,9 +6,11 @@ export class User {
     username: string;
     money: number;
     entry_date: string;
+    deactivated: boolean;
+    deactivated_since: string|null;
 
 
-    constructor(users_id: number, email: string, first_name: string, last_name: string, username: string, money: number, entry_date: string) {
+    constructor(users_id: number, email: string, first_name: string, last_name: string, username: string, money: number, entry_date: string, deactivated: boolean, deactivated_since: string|null) {
         this.users_id = users_id;
         this.email = email;
         this.first_name = first_name;
@@ -16,9 +18,11 @@ export class User {
         this.username = username;
         this.money = money;
         this.entry_date = entry_date;
+        this.deactivated = deactivated;
+        this.deactivated_since = deactivated_since;
     }
 
     static default(): User {
-        return new User(0, "N.A.", "N.A.", "N.A.", "N.A.", 0, "N.A.")
+        return new User(0, "N.A.", "N.A.", "N.A.", "N.A.", 0, "N.A.", false, "N.A.")
     }
 }
--- client/src/services/networking.service.ts
+++ client/src/services/networking.service.ts
@@ -9,11 +9,49 @@ import {GameHistory} from "../objects/game-history";
 })
 export class NetworkingService {
 
+    api: string = "http://localhost:7000/";
+
     constructor(private http: HttpClient) {
     }
 
+    getAccessToken(): string {
+        let access_token = sessionStorage.getItem("access_token");
+        if (!access_token) access_token = "";
+
+        return access_token;
+    }
+
+    getRefreshToken(): string {
+        let refresh_token = sessionStorage.getItem("refresh_token");
+        if (!refresh_token) refresh_token = "";
+
+        return refresh_token;
+    }
+
+    async validateAccessToken(): Promise<boolean> {
+        let observable = this.http.get<{ valid: boolean }>(this.api + "users/validate");
+        let authorized = false;
+        this.handle(await this.evaluate(observable), (result) => {
+            authorized = result.valid;
+        }, "AccessToken invalid!");
+
+        return authorized;
+    }
+
+    async refreshToken(): Promise<boolean> {
+        let observable = this.http.get<{ token: string }>(this.api + "users/refresh");
+        let token: string | undefined;
+        this.handle(await this.evaluate(observable), (result) => {
+            token = result.token;
+        }, "Refresh token invalid!");
+
+        if (token) sessionStorage.setItem("access_token", token)
+
+        return token !== undefined;
+    }
+
     getTestUser(): Observable<User> {
-        return of(new User(0, "jonaspfeifer@drei.at", "Jonas", "Pfeifer", "02c9cfda531c4464d1204547d744849c0642a0f622872c9504575a3ee51c9560", 27.99, "2022-01-01"))
+        return of(new User(0, "jonaspfeifer@drei.at", "Jonas", "Pfeifer", "02c9cfda531c4464d1204547d744849c0642a0f622872c9504575a3ee51c9560", 27.99, "2022-01-01", false, null))
     }
 
     getTestLastNGames(n: number): Observable<GameHistory[]> {
